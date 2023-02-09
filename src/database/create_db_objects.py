0f7ea6602c736c3682511532ae410be3f244cead
george <george27khan@gmail.com>
2023-02-09 15:09:03 +0600

    first

--- src/database/create_db_objects.py
+++ src/database/create_db_objects.py
@@ -3,10 +3,12 @@ from dotenv import load_dotenv
 
 from sqlalchemy import (
     create_engine,
+    insert,
     MetaData,
     Table,
     String,
     Integer,
+    Numeric,
     Column,
     Text,
     DateTime,
@@ -15,6 +17,7 @@ from sqlalchemy import (
     Index
 )
 from sqlalchemy.sql import func
+from sqlalchemy.orm import mapper
 from datetime import datetime
 
 """
@@ -50,8 +53,7 @@ engine = create_engine(
     f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
     #,echo=True
 )
-engine.connect()
-print(engine)
+conn = engine.connect()
 
 metadata = MetaData()
 
@@ -60,10 +62,10 @@ user = Table(
     metadata,
     Column("id", Integer(), primary_key=True),
     Column("telegram", String(1000), nullable=False, comment="username из telegram"),
-    Column("phone", String(1000), nullable=False, comment="Номер из telegram"),
+    Column("phone", String(20), nullable=False, comment="Номер из telegram"),
     Column("user_name", String(1000), nullable=False, comment="Имя из telegram"),
     Column("created_dt", DateTime(), server_default=func.now(), nullable=False),
-    Column("updated_on", DateTime(), server_default=func.now(), onupdate=func.now(), nullable=False),
+    Column("updated_on", DateTime(), server_default=func.now(), server_onupdate=func.now(), nullable=False),
     Index("idx_pk_user", "id"),
 )
 
@@ -74,7 +76,7 @@ scheduler = Table(
     Column("id_user", Integer(), ForeignKey(user.c.id), nullable=False),
     Column("visit_dt", DateTime(), nullable=False, comment="Дата и время бронирования"),
     Column("created_dt", DateTime(), server_default=func.now(), nullable=False),
-    Column("updated_on", DateTime(), server_default=func.now(), onupdate=func.now(), nullable=False),
+    Column("updated_on", DateTime(), server_default=func.now(), server_onupdate=func.now(), nullable=False),
     Index("idx_pk_scheduler", "id"),
     Index("idx_fk_user", "id_user"),
 )
@@ -82,29 +84,61 @@ position = Table(
     "position",
     metadata,
     Column("id", Integer(), primary_key=True),
-    Column("position_name", String(1000), nullable=False),
+    Column("position_name", String(500), nullable=False),
     Column("created_dt", DateTime(), server_default=func.now(), nullable=False),
-    Column("updated_on", DateTime(), server_default=func.now(), onupdate=func.now(), nullable=False),
+    Column("updated_on", DateTime(), server_default=func.now(), server_onupdate=func.now(), nullable=False),
     Index("idx_pk_position", "id"),
 )
 employee = Table(
     "employee",
     metadata,
     Column("id", Integer(), primary_key=True),
-    Column("first_name", String(1000), nullable=False),
-    Column("middle_name", String(1000), nullable=False),
-    Column("last_name", String(1000), nullable=False),
+    Column("first_name", String(100), nullable=False),
+    Column("middle_name", String(100), nullable=False),
+    Column("last_name", String(100), nullable=False),
     Column("birth_date", DateTime(), nullable=False),
     Column("email", String(1000), nullable=False),
-    Column("phone_number", String(1000), nullable=False),
+    Column("phone_number", String(20), nullable=False),
     Column("id_position", Integer(), ForeignKey(position.c.id), nullable=False),
     Column("is_male", Boolean(), nullable=False, comment="Пол"),
     Column("hire_date", DateTime(), server_default=func.now(), nullable=False),
     Column("created_dt", DateTime(), server_default=func.now(), nullable=False),
-    Column("updated_on", DateTime(), server_default=func.now(), onupdate=func.now(), nullable=False),
+    Column("updated_on", DateTime(), server_default=func.now(), server_onupdate=func.now(), nullable=False),
     Index("idx_pk_employee", "id"),
     Index("idx_fk_position", "id_position")
 )
+
+setting = Table(
+    "setting",
+    metadata,
+    Column("id", Integer(), primary_key=True),
+    Column("setting_code", String(100), nullable=False),
+    Column("setting_describe", String(1000), nullable=False),
+    Column("number_value", Numeric()),
+    Column("string_value", String(1000)),
+    Column("date_value", DateTime()),
+    Column("created_dt", DateTime(), server_default=func.now(), nullable=False),
+    Column("updated_on", DateTime(), server_default=func.now(), server_onupdate=func.now(), nullable=False),
+    #Index("idx_pk_settings", "id"),
+)
+
+
+class User(object):
+    pass
+class Scheduler(object):
+    pass
+class Position(object):
+    pass
+class Employee(object):
+    pass
+class Setting(object):
+    pass
+
+mapper(User, user)
+mapper(Scheduler, scheduler)
+mapper(Position, position)
+mapper(Employee, employee)
+mapper(Setting, setting)
 
 #отчистка базы от таблиц
 metadata.drop_all(engine)
@@ -112,3 +146,84 @@ metadata.drop_all(engine)
 #создание в базе всех описанных таблицы
 metadata.create_all(engine)
 
+setting_list = [
+    {
+        "setting_code": "session_time_hour",
+        "setting_describe": "Продолжительность приема в часах",
+        "number_value": 0.25
+    },
+    {
+        "setting_code": "monday_start_hour",
+        "setting_describe": "Начало рабочего дня в понедельник",
+        "number_value": 9
+    },
+    {
+        "setting_code": "tuesday_start_hour",
+        "setting_describe": "Начало рабочего дня в вторник",
+        "number_value": 9
+    },
+    {
+        "setting_code": "wednesday_start_hour",
+        "setting_describe": "Начало рабочего дня в среду",
+        "number_value": 9
+    },
+    {
+        "setting_code": "thursday_start_hour",
+        "setting_describe": "Начало рабочего дня в четверг",
+        "number_value": 9
+    },
+    {
+        "setting_code": "friday_start_hour",
+        "setting_describe": "Начало рабочего дня в пятницу",
+        "number_value": 9
+    },
+    {
+        "setting_code": "saturday_start_hour",
+        "setting_describe": "Начало рабочего дня в субботу",
+        "number_value": 10
+    },
+    {
+        "setting_code": "sunday_start_hour",
+        "setting_describe": "Начало рабочего дня в воскресенье",
+        "number_value": 10
+    },
+    {
+        "setting_code": "monday_end_hour",
+        "setting_describe": "Конец рабочего дня в понедельник",
+        "number_value": 18
+    },
+    {
+        "setting_code": "tuesday_end_hour",
+        "setting_describe": "Конец рабочего дня в вторник",
+        "number_value": 18
+    },
+    {
+        "setting_code": "wednesday_end_hour",
+        "setting_describe": "Конец рабочего дня в среду",
+        "number_value": 18
+    },
+    {
+        "setting_code": "thursday_end_hour",
+        "setting_describe": "Конец рабочего дня в четверг",
+        "number_value": 18
+    },
+    {
+        "setting_code": "friday_end_hour",
+        "setting_describe": "Конец рабочего дня в пятницу",
+        "number_value": 18
+    },
+    {
+        "setting_code": "saturday_end_hour",
+        "setting_describe": "Конец рабочего дня в субботу",
+        "number_value": 16
+    },
+    {
+        "setting_code": "sunday_end_hour",
+        "setting_describe": "Конец рабочего дня в воскресенье",
+        "number_value": 16
+    },
+]
+t = conn.begin() #start transaction
+res = conn.execute(insert(setting), setting_list)
+print(res.rowcount)
+t.commit()
